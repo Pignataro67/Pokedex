82011dddf329317ccfcff38f3797b05699147673
Pignataro67 <Pignataro67@gmail.com>
2021-11-26 07:35:32 -0800

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const poke_container = document.getElementById('poke-container')
+const pokemon_count = 150
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+}
